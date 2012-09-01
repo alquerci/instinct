a73dde069e6a1e9f2d43adf5177374447af32b76
alexandre.quercia <alexandre.quercia@programmer.net>
2012-09-01 08:53:39 +0200

    Add admin routing for write.

--- Controller/NewsController.php
+++ Controller/NewsController.php
@@ -19,6 +19,26 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
  * @author alexandre.quercia
  */
+/**
+ * Instinct - Application PHP using Symfony Framework
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (C) 2012  alexandre.quercia
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
+ * @author alexandre.quercia
+ */
 
 namespace Instinct\Bundle\NewsBundle\Controller;
 
@@ -61,19 +81,19 @@ class NewsController extends Controller
             throw $this->createNotFoundException('Page inexistante (page = '.$page.')');
         }
 
-            $entities = $rep->findBy(
-                array(),
-                array("date" => "desc"),
-            $nb_articles_page,
-            $offset);
+        $entities = $rep->findBy(
+            array(),
+            array("date" => "desc"),
+        $nb_articles_page,
+        $offset);
 
-            return $this->render("InstinctNewsBundle:News:index.html.twig",
-                array(
-                    "entities" => $entities,
-                    "page"     => $page,
-                    "page_prev"     => $page-1,
-                        "page_next"     => $page+1,
-                        "nb_pages" => $nb_pages,
-                    ));
+        return $this->render("InstinctNewsBundle:News:index.html.twig",
+            array(
+                "entities" => $entities,
+                "page"     => $page,
+                "page_prev"     => $page-1,
+                    "page_next"     => $page+1,
+                    "nb_pages" => $nb_pages,
+                ));
     }
 }
