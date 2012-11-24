978c7a0cff39ec70b5830f3de634dc43a7a185bf
alexandre.quercia <alexandrequercia@orange.fr>
2012-11-24 10:53:57 +0100

    Replace twig methodes for load js and style by autoloader with EventListener.

---
+++ EventListener/LoaderListener.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Instinct - Application PHP using Symfony Framework
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (C) 2012  alexandre.quercia
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
+ * @author alexandre.quercia
+ */
+
+namespace Instinct\Bundle\BbcodeBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+
+use Symfony\Bundle\TwigBundle\TwigEngine;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @author alexandre.quercia
+ * @since v0.0.1
+ */
+class LoaderListener implements EventSubscriberInterface
+{
+    protected $templating;
+
+    /**
+     * @since v0.0.1
+     *
+     * @param TwigEngine $templating
+     */
+    public function __construct(TwigEngine $templating)
+    {
+       $this->templating = $templating;
+    }
+
+    /**
+     * @since v0.0.1
+     *
+     * @param FilterResponseEvent $event
+     */
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $request = $event->getRequest();
+
+        // do not capture redirects or modify XML HTTP Requests
+        if ($request->isXmlHttpRequest()) {
+            return;
+        }
+
+        if ($response->isRedirection()
+            || ($response->headers->has('Content-Type') && false === strpos($response->headers->get('Content-Type'), 'html'))
+            || 'html' !== $request->getRequestFormat()
+        ) {
+            return;
+        }
+
+        $this->injectToolbar($response);
+    }
+
+    /**
+     * Injects the bbcode toolbar into the given Response.
+     *
+     * @param Response $response A Response instance
+     */
+    protected function injectToolbar(Response $response)
+    {
+        if (function_exists('mb_stripos')) {
+            $posrFunction   = 'mb_strripos';
+            $substrFunction = 'mb_substr';
+        } else {
+            $posrFunction   = 'strripos';
+            $substrFunction = 'substr';
+        }
+
+        $content = $response->getContent();
+        $pos = $posrFunction($content, '</body>');
+
+        if (false !== $pos) {
+            $toolbar = "\n".str_replace("\n", '', $this->templating->render(
+                'InstinctBbcodeBundle:Script:init.html.twig',
+                array(
+                )
+            ))."\n";
+            $content = $substrFunction($content, 0, $pos).$toolbar.$substrFunction($content, $pos);
+            $response->setContent($content);
+        }
+
+
+        $content = $response->getContent();
+        $pos = $posrFunction($content, '</head>');
+
+        if (false !== $pos) {
+            $toolbar = "\n".str_replace("\n", '', $this->templating->render(
+                'InstinctBbcodeBundle:Style:init.html.twig',
+                array(
+                )
+            ))."\n";
+            $content = $substrFunction($content, 0, $pos).$toolbar.$substrFunction($content, $pos);
+            $response->setContent($content);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::RESPONSE => array('onKernelResponse', 0),
+        );
+    }
+}
