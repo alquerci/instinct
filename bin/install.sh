882c291095229635ed9cba4934f0f4c7d1f60ba8
alquerci <alquerci@email.com>
2013-03-11 05:29:49 +0100

    Make some update

--- bin/install.sh
+++ bin/install.sh
@@ -15,6 +15,8 @@ fi;
 php -v > /dev/null || exit 1;
 php "$basedir/bin/composer.phar" self-update || exit 1;
 php "$basedir/bin/composer.phar" install || exit 1;
+php "$basedir/app/console" bbcode:dump || exit 1;
+
 echo -e "\n[info] For update vendors, type:\n\tphp bin/conposer.phar update";
 echo -e "Finally go to:\n\thttp://$(hostname)/_configurator/";
 exit 0;
